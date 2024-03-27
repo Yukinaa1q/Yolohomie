d0fc13d794928e078e8e7a77bc665add9c34b80e
nptakudo <thinh.nguyenph@hcmut.edu.vn>
2024-03-27 16:52:25 +0700

    stage 1 HW

---
+++ HW/send_mqtt.py
@@ -0,0 +1,52 @@
+#pip install paho-mqtt==1.6.1
+import paho.mqtt.client as mqtt
+import time
+
+MQTT_SERVER = "mqtt.ohstem.vn"
+MQTT_PORT = 1883
+MQTT_USERNAME = "thinhdadn"
+MQTT_PASSWORD = "hehe"
+MQTT_TOPIC_PUB = MQTT_USERNAME + "/feeds/V2"
+MQTT_TOPIC_SUB = MQTT_USERNAME + "/feeds/V2"
+
+
+def mqtt_connected(client, userdata, flags, rc):
+    print("Connected succesfully!!")
+    client.subscribe(MQTT_TOPIC_SUB)
+
+def mqtt_subscribed(client, userdata, mid, granted_qos):
+    print("Subscribed to Topic!!!")
+
+# # def mqtt_recv_message(client, userdata, message):
+#     print("Received: ", message.payload.decode("utf-8"))
+#     print(" Received message " + message.payload.decode("utf-8")
+#           + " on topic '" + message.topic
+#           + "' with QoS " + str(message.qos))
+
+def mqtt_published(client, userdata, mid):
+    print("Message published with mid: " + str(mid))
+
+mqttClient = mqtt.Client()
+mqttClient.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+mqttClient.connect(MQTT_SERVER, int(MQTT_PORT), 60)
+
+#Register mqtt events
+mqttClient.on_connect = mqtt_connected
+mqttClient.on_subscribe = mqtt_subscribed
+# mqttClient.on_message = mqtt_recv_message
+mqttClient.on_publish = mqtt_published
+
+mqttClient.loop_start()
+
+counter = 10
+while True:
+    time.sleep(1)
+    if counter == 0 :
+        counter = 10
+    counter -= 1   
+    if counter < 5 : 
+        fan = 1
+        mqttClient.publish(MQTT_TOPIC_PUB, fan)
+    else:
+        fan = 0
+        mqttClient.publish(MQTT_TOPIC_PUB, fan)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Yolohomie
 # we will modify this later
+
+##ohstem PJ link
+```
+https://app.ohstem.vn/#!/share/yolobit/2eGZi5dOFd1y0OnYwPC5tWBp1Qk
+```
